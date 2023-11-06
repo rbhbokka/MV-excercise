25e3f35dd697ebaa8d9283a39b72e34c8d3eccd8
Kiryl <kiryl.yemelyanenka@effective-soft.com>
2023-11-06 12:34:01 -0800

    Initial Commit

---
+++ recipes-excercise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  recipes-excercise
+//
+//  Created by Kirill Emelyanenko on 6.11.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ recipes-excercise/recipes_excerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipes_excerciseApp.swift
+//  recipes-excercise
+//
+//  Created by Kirill Emelyanenko on 6.11.23.
+//
+
+import SwiftUI
+
+@main
+struct recipes_excerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
